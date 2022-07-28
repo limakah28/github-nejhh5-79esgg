fa9e7d335aebd2c194fe4fc5215b351243c54e14
limakah28 <kauany.lima3@etec.sp.gov.br>
2022-07-28 14:54:51 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-nejhh5-79esgg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-nejhh5-79esgg)
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+})
+export class Tab1Page implements OnInit {
+  constructor(public pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonService.buscarTodosPokemon().subscribe((dados) => {});
+  }
+}
